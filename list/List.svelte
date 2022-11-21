5c204c9f19918e03c2f9d787cff7ae786e6753dc
Udo Bauer <knackwurstking.tux@gmail.com>
2022-11-21 12:43:48 +0100

    Handle list-item backgroud color

--- list/List.svelte
+++ list/List.svelte
@@ -25,7 +25,7 @@
   async function render(...data) {
     if (renderInterval) {
       clearInterval(renderInterval);
-      console.log(`Render data list with ${data.length} [CANCEL].`)
+      console.log(`Render data list with ${data.length} [CANCEL].`);
     }
 
     console.log(`Render data list with ${data.length} items [START]`);
@@ -37,19 +37,19 @@
       try {
         const item = renderItem(data.shift());
         if (item) {
-          item.classList.add("list-item")
+          item.classList.add("svelteui-list-item");
           ul.appendChild(item);
         }
       } catch (err) {
-        clearInterval(renderInterval)
-        console.log(`Render data list with ${data.length} [ABOARD]`)
-        console.warn(err)
-        return
+        clearInterval(renderInterval);
+        console.log(`Render data list with ${data.length} [ABOARD]`);
+        console.warn(err);
+        return;
       }
 
       if (!data.length) {
         clearInterval(renderInterval);
-        console.log(`Render data list with ${data.length} [FINISHED]`)
+        console.log(`Render data list with ${data.length} [FINISHED]`);
       }
     }, 1);
   }
@@ -60,6 +60,7 @@
 </script>
 
 <div
+  class="svelteui-list"
   bind:this={ul}
   on:click={(ev) => {
     /** @type {HTMLDivElement} */
@@ -68,7 +69,7 @@
     let el;
     // @ts-ignore
     for (el of (ev?.path || ev.composedPath() || [])) {
-      if (el.classList?.contains("list-item")) {
+      if (el.classList?.contains("svelteui-list-item")) {
         item = el;
         break;
       }
@@ -100,11 +101,28 @@
 </div>
 
 <style>
-  div {
+  :global(.svelteui-list) {
     margin: 0;
     padding: 0;
     position: relative;
     width: 100%;
     height: 100%;
+  }
+
+  :global(.svelteui-list-item) {
+    padding: 16px 16px;
+    transition: background-color 0.25s ease;
+  }
+
+  :global(.svelteui-list-item:hover) {
+    background-color: var(--theme-secondary-hover);
+  }
+
+  :global(.svelteui-list-item:active) {
+    background-color: var(--theme-secondary-focus);
+  }
+
+  :global(.svelteui-list-item.selected) {
+    background-color: var(--theme-primary-focus);
   }
 </style>
