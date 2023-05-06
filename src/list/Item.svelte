3a96a7622ed5cf9f8b2ebd5169916690c90e31e4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-06 10:46:20 +0200

    Update list item styles (@todo: list item select handling)

--- src/list/Item.svelte
+++ src/list/Item.svelte
@@ -24,25 +24,28 @@
     list-style-type: none;
     display: flex;
     flex-direction: row;
+    font-size: 1rem;
+    overflow: hidden;
   }
 
   .custom-list-item__text {
     height: 100%;
-    padding: 4px 8px;
+    padding: 8px;
 
     display: inline-flex;
     flex-direction: column;
     align-items: flex-start;
-    justify-content: center;
-
-    border: 1px solid green;
+    justify-content: flex-end;
   }
 
   .custom-list-item__text .custom-list-item__primary-text {
-    font-size: 1rem;
+    font-size: 1em;
+    font-weight: 400;
+    font-family: FiraCode;
   }
 
   .custom-list-item__text .custom-list-item__secondary-text {
-    font-size: 0.8rem;
+    font-size: 0.85em;
+    font-weight: 400;
   }
 </style>
--- src/list/Meta.svelte
+++ src/list/Meta.svelte
@@ -8,13 +8,7 @@
 <style>
   .custom-list-item__meta {
     width: 100%;
-    padding: 4px 8px;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-
-    border: 1px solid red;
+    height: 100%;
+    padding: 8px;
   }
 </style>
