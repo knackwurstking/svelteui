6206333afc55ebc0eb20a078973c0f027b68f5b2
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-08 21:10:22 +0200

    Adding drawer item (unfinished)

---
+++ src/drawer/Drawer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  /** swipeable will enable open/close per swipe (touch) */
+  export let swipeable: boolean = false;
+  export let width: number = 300;
+</script>
+
+<div
+  class="custom-drawer"
+  class:custom-drawer__swipeable={swipeable}
+  style={`
+    width: ${300}px;
+    left: -${300}px;
+  `}
+>
+  <slot />
+</div>
+
+<style>
+  .custom-drawer {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    transition: left 0.25s ease;
+  }
+
+  .custom-drawer__swipeable {
+  }
+</style>
