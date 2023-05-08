6c0bf5f6b175fcc567cf3b9ce173c8ae43b8486a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-08 19:48:02 +0200

    Adding some files for demo frontend

---
+++ src/_App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  // TODO: showcase (and testing) all the components here
+  import { Router, Link, Route } from "svelte-routing";
+  import Buttons from "./_routes/buttons/Buttons.svelte";
+  import Checkboxes from "./_routes/checkboxes/Checkboxes.svelte";
+  import Lists from "./_routes/lists/Lists.svelte";
+  import Misc from "./_routes/misc/Misc.svelte";
+  import Sliders from "./_routes/sliders/Sliders.svelte";
+</script>
+
+<Router>
+  <!-- TODO: a drawer component here to choose which route to go? -->
+  <div>
+    <!-- TODO: routes -->
+  </div>
+</Router>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./theme/custom/main.css";
+import App from "./_App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
