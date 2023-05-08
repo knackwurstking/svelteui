0ae64e6a7cbf7c9d2e50dab12646f63c7d59158e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-08 20:58:11 +0200

    Adding drawer component (@todo)

---
+++ src/slider/drawer/Drawer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  /** swipeable will enable open/close per swipe (touch) */
+  export let swipeable: boolean = false;
+</script>
+
+<div class="custom-drawer" class:custom-drawer__swipeable={swipeable}>
+  <slot />
+</div>
+
+<style>
+  .custom-drawer {
+    position: absolute;
+  }
+
+  .custom-drawer__swipeable {
+  }
+</style>
