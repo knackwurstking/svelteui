b2fbfb712f8390245744f3faeacc5849c4990023
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-08 19:51:05 +0200

    Moving _routes to _demo dir

--- src/_App.svelte
+++ src/_App.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   // TODO: showcase (and testing) all the components here
   import { Router, Link, Route } from "svelte-routing";
-  import Buttons from "./_routes/buttons/Buttons.svelte";
-  import Checkboxes from "./_routes/checkboxes/Checkboxes.svelte";
-  import Lists from "./_routes/lists/Lists.svelte";
-  import Misc from "./_routes/misc/Misc.svelte";
-  import Sliders from "./_routes/sliders/Sliders.svelte";
+  import Buttons from "./_demo/routes/buttons/Buttons.svelte";
+  import Checkboxes from "./_demo/routes/checkboxes/Checkboxes.svelte";
+  import Lists from "./_demo/routes/lists/Lists.svelte";
+  import Misc from "./_demo/routes/misc/Misc.svelte";
+  import Sliders from "./_demo/routes/sliders/Sliders.svelte";
 </script>
 
 <Router>
   <!-- TODO: a drawer component here to choose which route to go? -->
   <div>
-    <!-- TODO: routes -->
+    <!-- TODO: routes (showcase components in a iframe?) -->
   </div>
 </Router>
 
