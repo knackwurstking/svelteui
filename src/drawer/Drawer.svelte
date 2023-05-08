fe5d28057d1df12e49e3f5bd07188aaf1e675f40
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-08 22:08:26 +0200

    Adding fixed prop

--- src/drawer/Drawer.svelte
+++ src/drawer/Drawer.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+  // NOTE: move drawer (swipeable) per transform
+
   /** swipeable will enable open/close per swipe (touch) */
   export let swipeable: boolean = false;
   export let width: number = 300;
+  /** fixed will disable swipeable */
+  export let fixed: boolean = false;
 </script>
 
 <div
   class="custom-drawer"
-  class:custom-drawer__swipeable={swipeable}
+  class:custom-drawer__swipeable={!fixed && swipeable}
+  class:custom-drawer__fixed={fixed}
   style={`
     width: ${width}px;
     left: -${width}px;
@@ -21,9 +26,13 @@
     position: absolute;
     top: 0;
     bottom: 0;
-    transition: left 0.25s ease;
+    transition: transform 0.25s ease;
   }
 
   .custom-drawer__swipeable {
+  }
+
+  .custom-drawer__fixed {
+    left: 0 !important;
   }
 </style>
