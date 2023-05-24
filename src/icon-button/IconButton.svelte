2e72175564288fec01c04b1e60add33c77616a82
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-24 20:35:44 +0200

    Adding and IconButton component (@todo: add ripple effect)

---
+++ src/icon-button/IconButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function _click() {
+    dispatch("click");
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<span class="custom-icon-button" on:click={_click}>
+  <slot />
+</span>
