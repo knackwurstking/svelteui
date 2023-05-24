cb6a9846ecb07bf2ad6a8581f0527354664fa24b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-24 20:51:04 +0200

    Adding icon button component

--- src/icon-button/IconButton.svelte
+++ src/icon-button/IconButton.svelte
@@ -3,12 +3,19 @@
 
   const dispatch = createEventDispatcher();
 
+  let _class = "";
+  export { _class as class };
+
   function _click() {
     dispatch("click");
   }
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<span class="custom-icon-button" on:click={_click}>
+<span
+  class={"custom-icon-button material-symbols-outlined " + _class}
+  {...$$restProps}
+  on:click={_click}
+>
   <slot />
 </span>
