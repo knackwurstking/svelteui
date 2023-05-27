fe8304bba0423fd2fed4c287617024f0ec34a6a3
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-27 14:18:45 +0200

    progress on infinite swipe container

--- infinite-swipe/InfiniteSwipe.svelte
+++ infinite-swipe/InfiniteSwipe.svelte
@@ -1,3 +1,14 @@
 <script lang="ts">
   export let orientation: "horizontal" | "vertical";
 </script>
+
+<div class="custom-infinite-swipe">
+  <slot />
+</div>
+
+<style>
+  .custom-infinite-swipe {
+    overflow: hidden;
+    scroll-snap-type: x mandatory;
+  }
+</style>
---
+++ infinite-swipe/Item.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="custom-infinite-swipe__item">
+  <slot />
+</div>
