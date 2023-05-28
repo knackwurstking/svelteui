06c49936c33c5556d609c5e2c72217a120db073c
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-28 20:20:12 +0200

    debug pointer move event

--- infinite-swipe/InfiniteSwipe.svelte
+++ infinite-swipe/InfiniteSwipe.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
   export let orientation: "horizontal" | "vertical";
+
+  let swipeContainer: HTMLDivElement;
+
+  $: {
+    if (swipeContainer) {
+      // TODO: add pointer event handlers (move if button !== 0)
+      swipeContainer.onpointermove = (ev) => {
+        const clientX = ev.clientX;
+        const clientY = ev.clientY;
+        console.debug(`clientX=${clientX}, clientY=${clientY}`);
+      };
+    }
+  }
 </script>
 
-<div class="custom-infinite-swipe">
+<div bind:this={swipeContainer} class="custom-infinite-swipe">
   <slot />
 </div>
 
