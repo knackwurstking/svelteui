d4c58c84ff544bf1dcd37ba5ea85d2dfe0085ded
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-03 01:51:11 +0200

    move topappbar styles to theme

--- top-app-bar/TopAppBar.svelte
+++ top-app-bar/TopAppBar.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+  let _css = "";
+  export { _css as css };
+
   // TopAppBar.svelte
   export let height: number = 50;
   // TODO: need a slot for meta "left" and "right"
 </script>
 
 <nav
+  class={`custom-topappbar ` + _css}
   style={`
     width: 100%;
     height: ${height}px;
@@ -12,9 +16,3 @@
 >
   <!-- TODO: left and right meta slots -->
 </nav>
-
-<style>
-  nav {
-    border-bottom: 1px solid var(--theme-border-color);
-  }
-</style>
