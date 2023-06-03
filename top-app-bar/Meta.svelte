9268b215447be79a632f4bb55f17a313a0959c7f
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-03 02:46:58 +0200

    add rest props and update _css prop

--- top-app-bar/Meta.svelte
+++ top-app-bar/Meta.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
   let _css = "";
   export { _css as css };
-
-  let _styles = "";
-  export { _styles as styles };
 </script>
 
 <div 
-  class={"custom-topappbar-meta " + _class}
+  class={"custom-topappbar-meta " + _css}
+  {...$$restProps}
 >
   <slot />
 </div>
