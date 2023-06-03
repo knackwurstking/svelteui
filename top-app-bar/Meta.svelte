dce9f5a0aadf3979c9fabcd6167174d620cbabb4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-03 02:00:12 +0200

    add meta left/right

--- top-app-bar/Meta.svelte
+++ top-app-bar/Meta.svelte
@@ -1,3 +1,11 @@
 <script lang="ts">
-  // Meta.svelte
+  let _css = "";
+  export { _css as css };
+
+  let _styles = "";
+  export { _styles as styles };
 </script>
+
+<div 
+  class={"custom-topappbar-meta " + _class}
+></div>
--- top-app-bar/TopAppBar.svelte
+++ top-app-bar/TopAppBar.svelte
@@ -5,18 +5,17 @@
   let _styles = "";
   export { _styles as styles };
 
-  // TopAppBar.svelte
   export let height: number = 50;
-  // TODO: need a slot for meta "left" and "right"
 </script>
 
 <nav
-  class={`custom-topappbar ` + _css}
+  class={"custom-topappbar " + _css}
   style={`
     width: 100%;
     height: ${height}px;
   ` + _styles}
   {...$$restProps}
 >
-  <!-- TODO: left and right meta slots -->
+  <slot name="left" />
+  <slot name="right" />
 </nav>
