680022b89c28522de95eabc1cd14877f8e3e2957
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-04 06:47:20 +0200

    add label class: 'noedit' and 'disabled'

--- input/Input.svelte
+++ input/Input.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+  export let noedit = false;
+  export let disabled = false;
 </script>
 
-<label class="custom-input">
+<label
+  class="custom-input"
+  class:noedit
+  class:disabled
+>
   <span><slot /></span>
   <input />
 </label>
 
 <style>
   .custom-input {
+  }
+
+  .custom-input.noedit {
+  }
+
+  .custom-input.disabled {
   }
 </style>
