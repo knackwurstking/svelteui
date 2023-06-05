37c204797e9802b9c094c9b0af0232ccdad887b2
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-05 08:06:46 +0200

    input number component

---
+++ input-number/InputNumber.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let value: number;
+
+  export let min:number = null;
+  export let max:number = null;
+  export let noedit = false;
+  export let disabled = false;
+</script>
+
+<label
+  class="custom-input"
+  class:noedit
+  class:disabled
+>
+  <span><slot /></span>
+  <input bind:value type="number" {min} {max} />
+</label>
+
+<style>
+  .custom-input[type="number"] {
+  }
+
+  .custom-input[type="number"].noedit {
+  }
+
+  .custom-input[type="number"].disabled {
+  }
+</style>
