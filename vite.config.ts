c95e0f49b9e506cc3dfb521379427601e505972d
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-15 10:20:04 +0200

    remove todo

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// TODO: move the public/ dir into src/
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
